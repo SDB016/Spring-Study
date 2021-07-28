0935c363b3839f1fac78c80d72e40120cd21c046
SDB016 <dongbin4013@naver.com>
2021-07-28 15:02:39 +0900

    MemberRepository CRUD test

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.Entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,4 +30,33 @@ class MemberRepositoryTest {
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
     }
 
+    @Test
+    void basicCRUD() {
+        Member memberA = new Member("dongbin");
+        Member memberB = new Member("ehdqls");
+
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        //단건 조회 검증
+        Member findA = memberRepository.findById(memberA.getId()).get();
+        Member findB = memberRepository.findById(memberB.getId()).get();
+        assertThat(findA).isEqualTo(memberA);
+        assertThat(findB).isEqualTo(memberB);
+
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertTrue(all.contains(memberA));
+        assertTrue(all.contains(memberB));
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepository.delete(memberA);
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(1);
+    }
 }
