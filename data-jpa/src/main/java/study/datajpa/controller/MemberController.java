df2fa0aa8c5538294f87b82027c49bd380f2d9ca
SDB016 <dongbin4013@naver.com>
2021-07-30 21:23:36 +0900

    페이징과 정렬

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -2,11 +2,15 @@ package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.EventPublishingRunListener;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import study.datajpa.Entity.Member;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.repository.MemberRepository;
 
 import javax.annotation.PostConstruct;
@@ -20,10 +24,7 @@ public class MemberController {
     @GetMapping("/member/{id}")
     public String getMember(@PathVariable("id") Long id) {
         Member member = memberRepository.findById(id).get();
-        if (member != null) {
-            return member.getUsername();
-        }
-        return "Id is Error";
+        return member.getUsername();
     }
 
     @GetMapping("/member2/{id}")
@@ -33,6 +34,21 @@ public class MemberController {
 
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("memberA"));
+        for (int i = 1; i < 101; i++) {
+            memberRepository.save(new Member("member" + i, 80 - i / 2));
+        }
+    }
+
+    @GetMapping("/members")
+    public Page<Member> list(@PageableDefault(size = 4, sort = "username") Pageable pageable) {
+        Page<Member> pageList = memberRepository.findAll(pageable);
+        return pageList;
+    }
+
+    @GetMapping("/membersDto")
+    public Page<Member> list(@PageableDefault(size = 4, sort = "username") Pageable pageable) {
+        Page<MemberDto> memberDtos = memberRepository.findAll(pageable).map(m -> new MemberDto(m.getId(), m.getUsername(), null));
+        return memberDtos;
     }
 }
+
